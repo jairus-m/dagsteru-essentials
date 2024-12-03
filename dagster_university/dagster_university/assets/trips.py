d4637293fd5ff3fc6308ea2f1f589401294c05ca
jairus-m <jairusmartinez@gmail.com>
2024-12-02 16:15:29 -0800

    feat: add paritions + update descriptions

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -1,23 +1,36 @@
-import duckdb
+from dagster_duckdb import DuckDBResource
+from dagster import asset, AssetExecutionContext
 import os
 import requests
 from . import constants
-from dagster import asset
+from dagster import asset, MetadataValue, MaterializeResult
 
-@asset
-def taxi_trips_file() -> None:
-    """
+import pandas as pd
+
+from ..partitions import monthly_partition
+
+@asset(
+    partitions_def=monthly_partition,
+    group_name="raw_files"
+)
+def taxi_trips_file(context) -> None:
+  """
       The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
-    """
-    month_to_fetch = '2023-03'
-    raw_trips = requests.get(
-        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
-    )
+  """
 
-    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
-        output_file.write(raw_trips.content)
+  partition_date_str = context.partition_key
+  month_to_fetch = partition_date_str[:-3]
 
-@asset
+  raw_trips = requests.get(
+      f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+  )
+
+  with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+      output_file.write(raw_trips.content)
+
+@asset(
+    group_name="raw_files",
+)
 def taxi_zones_file() -> None:
     """
       The raw csv files for the taxi zones dataset. Sourced from the NYC Open Data portal.
@@ -30,36 +43,49 @@ def taxi_zones_file() -> None:
         output_file.write(raw_taxi_zones.content)
 
 @asset(
-    deps=["taxi_trips_file"]
+  deps=["taxi_trips_file"],
+  partitions_def=monthly_partition,
 )
-def taxi_trips() -> None:
+def taxi_trips(context: AssetExecutionContext, database: DuckDBResource) -> None:
     """
-      The raw taxi trips dataset, loaded into a DuckDB database
-    """
-    sql_query = """
-        create or replace table trips as (
-          select
-            VendorID as vendor_id,
-            PULocationID as pickup_zone_id,
-            DOLocationID as dropoff_zone_id,
-            RatecodeID as rate_code_id,
-            payment_type as payment_type,
-            tpep_dropoff_datetime as dropoff_datetime,
-            tpep_pickup_datetime as pickup_datetime,
-            trip_distance as trip_distance,
-            passenger_count as passenger_count,
-            total_amount as total_amount
-          from 'data/raw/taxi_trips_2023-03.parquet'
-        );
+      The raw taxi trips dataset, loaded into a DuckDB database, partitioned by month.
     """
 
-    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
-    conn.execute(sql_query)
+    partition_date_str = context.partition_key
+    month_to_fetch = partition_date_str[:-3]
+
+    query = f"""
+      create table if not exists trips (
+        vendor_id integer, pickup_zone_id integer, dropoff_zone_id integer,
+        rate_code_id double, payment_type integer, dropoff_datetime timestamp,
+        pickup_datetime timestamp, trip_distance double, passenger_count double,
+        total_amount double, partition_date varchar
+      );
+
+      delete from trips where partition_date = '{month_to_fetch}';
+
+      insert into trips
+      select
+        VendorID, PULocationID, DOLocationID, RatecodeID, payment_type, tpep_dropoff_datetime,
+        tpep_pickup_datetime, trip_distance, passenger_count, total_amount, '{month_to_fetch}' as partition_date
+      from '{constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)}';
+    """
+
+    with database.get_connection() as conn:
+        conn.execute(query)
+    
+    num_rows = len(pd.read_parquet(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch)))
+
+    return MaterializeResult(
+        metadata={
+            'Number of records': MetadataValue.int(num_rows)
+        }
+    )
 
 @asset(
     deps=["taxi_zones_file"]
 )
-def taxi_zones() -> None:
+def taxi_zones(database: DuckDBResource) -> None:
     """
       The raw taxi zones dataset, loaded into a DuckDB database
     """
@@ -74,6 +100,6 @@ def taxi_zones() -> None:
         );
     """
 
-    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
-    conn.execute(sql_query)
+    with database.get_connection() as conn:
+      conn.execute(sql_query)
     
